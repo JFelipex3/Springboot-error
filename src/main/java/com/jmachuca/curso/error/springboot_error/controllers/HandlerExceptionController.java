e22bbb6b0a1edd46fe3237f8a279645b74a6fcc9
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-03 23:03:10 -0400

    Manejo error 500 por Number Exception usando otra forma de manejar los errores

--- src/main/java/com/jmachuca/curso/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/jmachuca/curso/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.jmachuca.curso.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -41,5 +44,20 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.NOT_FOUND.value());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception ex) {
+
+        Map<String, Object> error = new HashMap<>();
+
+        error.put("date", new Date());
+        error.put("error", "numero invalido o incorrecto, no tiene formato de digito");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
+
     }
 }
