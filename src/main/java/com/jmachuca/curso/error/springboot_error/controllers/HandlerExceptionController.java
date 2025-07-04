d880e6bbea7539d4a3a900f0478e58ff6fd0fa1d
JFelipex3 <jfelipe.machuca@gmail.com>
2025-07-03 22:54:10 -0400

    Manejo excepcion 404

--- src/main/java/com/jmachuca/curso/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/jmachuca/curso/error/springboot_error/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.jmachuca.curso.error.springboot_error.models.Error;
 
@@ -27,5 +28,18 @@ public class HandlerExceptionController {
 
         // Forma #2
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException ex) {
+
+        Error error = new Error();
+
+        error.setDate(new Date());
+        error.setError("Api rest no encontrada");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 }
